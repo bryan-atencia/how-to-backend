e227c76b4641ad74f51575ad9bd4e8eff4e32bdc
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-10 21:49:24 -0500

    updates?

--- config/database.js
+++ config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        // client: 'postgres',
+        client:"sqlite",
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: "ec2-18-210-214-86.compute-1.amazonaws.com",
+        port:  5432,
+        database: 'd3uqo4trcp0fe6',
+        username: 'zlcnnzcmujkifl',
+        password: '40d502914957071340f82330f435890113cdc0c0c91fc81abe81679195f3a2d7',
+        url: "postgres://zlcnnzcmujkifl:40d502914957071340f82330f435890113cdc0c0c91fc81abe81679195f3a2d7@ec2-18-210-214-86.compute-1.amazonaws.com:5432/d3uqo4trcp0fe6"
       },
       options: {
         ssl: false,
