3a81da2f07c8faf9f81e9ec625f0c7ff293ec9fa
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-13 12:39:55 -0500

    update db

--- config/database.js
+++ config/database.js
@@ -4,17 +4,21 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client:"sqlite",
-        host: "ec2-18-210-214-86.compute-1.amazonaws.com",
+        client:"postgres",
+        host: "localhost",
         port:  5432,
-        database: 'd3uqo4trcp0fe6',
-        username: 'zlcnnzcmujkifl',
-        password: '40d502914957071340f82330f435890113cdc0c0c91fc81abe81679195f3a2d7',
-        url: "postgres://zlcnnzcmujkifl:40d502914957071340f82330f435890113cdc0c0c91fc81abe81679195f3a2d7@ec2-18-210-214-86.compute-1.amazonaws.com:5432/d3uqo4trcp0fe6"
+        database: 'bryanatencia',
+        username: 'bryanatencia'
       },
-      options: {
-        ssl: false,
-      },
-    },
-  },
+      options:{
+        pool: {
+          min: 0,
+          max: 15,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000
+        }
+      }
+    }
+  }
 });
