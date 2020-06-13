fb90bc5de36d00a903de3c78c7666045058d8ff6
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-13 12:48:09 -0500

    poduction db build

--- config/database.js
+++ config/database.js
@@ -5,10 +5,16 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client:"postgres",
-        host: "localhost",
-        port:  5432,
-        database: 'bryanatencia',
-        username: 'bryanatencia'
+        // host: "localhost",
+        // port:  5432,
+        // database: 'bryanatencia',
+        // username: 'bryanatencia'
+        host:"ec2-18-210-214-86.compute-1.amazonaws.com",
+        port:5432,
+        database:"d3uqo4trcp0fe6",
+        username:'zlcnnzcmujkifl',
+        password: '40d502914957071340f82330f435890113cdc0c0c91fc81abe81679195f3a2d7',
+        url: "postgres://zlcnnzcmujkifl:40d502914957071340f82330f435890113cdc0c0c91fc81abe81679195f3a2d7@ec2-18-210-214-86.compute-1.amazonaws.com:5432/d3uqo4trcp0fe6"
       },
       options:{
         pool: {
