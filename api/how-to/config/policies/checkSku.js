b55be1b23870f6acb8a3224d1df3f49feac4f458
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-24 11:43:52 -0500

    adjusting policies

--- api/how-to/config/policies/checkSku.js
+++ api/how-to/config/policies/checkSku.js
@@ -4,6 +4,7 @@ module.exports = async (ctx, next) => {
  // The code below will be executed after the controller's action.
  if (ctx.status === 200) {
    console.log(ctx, 'the ctx variable')
-   ctx.body = 'We cannot find the resource.';
+   // ctx.body = 'We cannot find the resource.';
+   return ctx
  }
 };
