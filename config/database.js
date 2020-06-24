93942de129d93675cc99faf4c0f2c3fc7ec2f652
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-24 11:30:51 -0500

    adding hook

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client:"postgres",
+
         // host: "localhost",
         // port:  5432,
         // database: 'bryanatencia',
